9ee868d000246242511018309633e3804dbed3f4
M. Novrizal Faros <mnovrizalf@gmail.com>
2016-03-30 10:32:28 +0700

    Added files via upload

---
+++ Jurnal2.java
@@ -0,0 +1,44 @@
+package jurnal2;
+
+import java.util.Scanner;
+
+public class Jurnal2 {
+
+    public static void main(String[] args) {  
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Masukkan Angka : ");
+        int n=scnr.nextInt();
+        int l = 1;
+        int tingkat = 10;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int k=n;k>i;k--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=l;j++)
+            {
+                System.out.print("+");
+            }
+            l+=2;
+            System.out.println("");
+        }
+        l -= 4;
+ 
+        for(int i=(n-1);i>=1;i--)
+        {
+            for(int k=i;k<=(n-1);k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=l;j>=1;j--)
+            {
+                System.out.print("+");
+            }
+            l-=2;
+            System.out.println("");
+        }
+    }
+    
+}
